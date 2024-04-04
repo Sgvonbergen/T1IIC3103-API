f40b1c29dcee72ab7dd7ab6e0ab2899fd13aa061
Sebasthian von Bergen <sgvonbergen@uc.cl>
2024-04-04 18:09:56 -0300

    set ssl: false

--- database.js
+++ database.js
@@ -7,9 +7,7 @@ async function connectDB() {
         database: process.env.DATABASE_NAME,
         username: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
-        ssl: {
-            rejectUnauthorized: false
-        }
+        ssl: false
     });
     await client.connect();
 
